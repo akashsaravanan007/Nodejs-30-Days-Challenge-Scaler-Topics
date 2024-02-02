eba552cef64a4e3d0011eccaf74034b64bd0b5f8
akashsaravanan007 <58935531+akashsaravanan007@users.noreply.github.com>
2024-02-02 12:22:27 +0530

    second commit

---
+++ Question2.js
@@ -0,0 +1,18 @@
+import * as fs from "fs";
+
+function writeToFile(filePath, content) {
+  fs.writeFile(filePath, content, (err) => {
+    if (err) {
+      console.error(`Error writing to file: ${err.message}`);
+    } else {
+      console.log(`Data written to ${filePath}`);
+    }
+  });
+}
+
+// Test Cases
+writeToFile("test-files/output1.txt", "Sample content.");
+writeToFile(
+  "test-files/nonexistent-folder/output.txt",
+  "Content in a non-existent folder."
+);
