ebe2da6291f408c83c6a79ffc613469fa399a177
Akash S <58935531+akashsaravanan007@users.noreply.github.com>
2024-02-13 11:48:15 +0530

    Delete Day 11 directory

--- Day 11/jwtUtils.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const JWT_SECRET_KEY = "xhdfndndg";
-
-function generateToken(payload) {
-  return jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: "1h" });
-}
-
-module.exports = { generateToken };
